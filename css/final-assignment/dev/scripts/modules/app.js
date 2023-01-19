6df0188869ca76a9a8b0a3d4a3e7d80fb3cad7d2
Sitaram Oli <sitaramoli1998@gmail.com>
2023-01-19 12:21:54 +0545

    final

--- css/final-assignment/dev/scripts/modules/app.js
+++ css/final-assignment/dev/scripts/modules/app.js
@@ -1,12 +1,10 @@
 import 'popper.js';
-// import 'bootstrap/js/dist/button';
-import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/button';
 import 'bootstrap/js/dist/dropdown';
-// import 'bootstrap/js/dist/modal';
-// import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/tab';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/offcanvas';
-// import 'bootstrap-select';
+import 'bootstrap-select';
 import 'bootstrap/js/dist/modal';
 
 
