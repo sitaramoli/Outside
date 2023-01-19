c95bf3f9d2b43bcb6a0c6380f4e4f89b7b3940b3
Sitaram Oli <sitaramoli1998@gmail.com>
2023-01-19 16:36:20 +0545

    final project added

--- css/final-assignment/dev/scripts/modules/app.js
+++ css/final-assignment/dev/scripts/modules/app.js
@@ -4,6 +4,7 @@ import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/tab';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/offcanvas';
+import 'bootstrap/js/dist/carousel';
 import 'bootstrap-select';
 import 'bootstrap/js/dist/modal';
 
