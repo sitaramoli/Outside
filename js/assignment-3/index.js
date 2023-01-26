6ccadeb9c37595d658c031f97b60230fa6322c2a
Sitaram Oli <sitaramoli1998@gmail.com>
2023-01-26 08:32:27 +0545

    carousel completed

---
+++ js/assignment-3/index.js
@@ -0,0 +1,89 @@
+// 
+function startCarousel() {
+    let carouselContainer = document.querySelector(".carousel-container");
+    let carouselImageWrapper = document.querySelector(".carousel-image-wrapper");
+    let currentIndex = 1;
+    let slideLength = carouselImageWrapper.children.length;
+    let holdingTime = 4000; /*in milliseconds*/
+
+    // control buttons
+    function createCarouselButtons() {
+        // prev btn
+        let prevBtn = document.createElement("button");
+        prevBtn.setAttribute("id", "prev");
+        prevBtn.classList.add("carousel-control-button");
+        prevBtn.addEventListener("click", () => showPrevSlide());
+        prevBtn.append("❮");
+
+        // next btn
+        let nextBtn = document.createElement("button");
+        nextBtn.setAttribute("id", "next");
+        nextBtn.classList.add("carousel-control-button");
+        nextBtn.addEventListener("click", () => showNextSlide());
+        nextBtn.append("❯");
+
+        carouselContainer.append(prevBtn);
+        carouselContainer.append(nextBtn);
+    }
+
+    // indicators
+    function createCarouselIndicators() {
+        let carouselIndicators = document.createElement("div");
+        carouselIndicators.classList.add("carousel-indicators");
+        carouselContainer.append(carouselIndicators);
+        for (let i = 0; i < slideLength; i++) {
+            let indicator = document.createElement("button");
+            indicator.classList.add("carousel-indicator-btn");
+            carouselIndicators.append(indicator);
+            indicator.addEventListener("click", function () {
+                showIndicatorSlide(i + 1);
+            });
+        }
+    }
+
+    // periodic slide
+    function startPeriodicSlide() {
+        showNextSlide();
+        setTimeout(startPeriodicSlide, holdingTime);
+    }
+
+    // indicator slide
+    function showIndicatorSlide(index) {
+        currentIndex = index;
+        showSlide();
+    }
+
+    //prev slide 
+    function showPrevSlide() {
+        if (currentIndex === 1) {
+            currentIndex = slideLength;
+        }
+        else {
+            currentIndex--;
+        }
+        showSlide();
+
+    }
+
+    // next slide
+    function showNextSlide() {
+        if (currentIndex === slideLength) {
+            currentIndex = 1;
+        }
+        else {
+            currentIndex++;
+        }
+        showSlide();
+    }
+
+    // show slide
+    function showSlide() {
+        carouselImageWrapper.style.transform = `translateX(${-(currentIndex - 1) * 100}vw)`;
+    }
+
+    createCarouselIndicators();
+    createCarouselButtons();
+    setTimeout(startPeriodicSlide,holdingTime); /*start slide first time after holdingTime*/ 
+}
+
+startCarousel();
