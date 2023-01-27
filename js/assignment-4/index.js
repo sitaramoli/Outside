cc1e4193802d8a9f1c456e50221ae202396709c4
Sitaram Oli <sitaramoli1998@gmail.com>
2023-01-28 00:52:51 +0545

    checks overlap while generating boxes

--- js/assignment-4/index.js
+++ js/assignment-4/index.js
@@ -6,13 +6,13 @@ const BOX_COUNT = 2;
 const SPEED = 10;
 
 function main() {
-    const body = document.getElementsByTagName("body")[0];
+    const body = document.querySelector("body");
     const boxList = initializeApp(body);
     startApp(boxList);
 }
 
-function initializeApp(body) {
-    const boxContainer = createBoxContainer(body);
+function initializeApp(parent) {
+    const boxContainer = createBoxContainer(parent);
     const boxList = createBoxes(boxContainer);
     return boxList;
 }
@@ -34,9 +34,9 @@ function createBoxContainer(body) {
 /**
  *creates multiple boxes
  *
- * @param {*} boxContainer
+ * @param {*} parent
  */
-function createBoxes(boxContainer) {
+function createBoxes(parent) {
     let boxList = [];
     for (let i = 0; i < BOX_COUNT; i++) {
         const box = document.createElement("div");
@@ -46,66 +46,65 @@ function createBoxes(boxContainer) {
         box.style.height = `${BOX_HEIGHT}px`;
         box.style.width = `${BOX_WIDTH}px`;
 
-        let topPosition = getPosition(CONTAINER_HEIGHT);
-        let leftPosition = getPosition(CONTAINER_WIDTH);
+        let topPosition = getPosition(CONTAINER_HEIGHT, BOX_HEIGHT);
+        let leftPosition = getPosition(CONTAINER_WIDTH, BOX_WIDTH);
 
-        // check if box initially goes outside the container and box touches the container border
+        //if generated box touches the border initially, 
+        // move one step up/down so that box will move for at least one step
         if (topPosition == 0) {
-            topPosition = topPosition + BOX_HEIGHT;
+            topPosition = topPosition + SPEED;
         }
-        else if (topPosition == CONTAINER_HEIGHT) {
-            topPosition = topPosition - BOX_HEIGHT - SPEED;
-        }
-        else if (topPosition + BOX_HEIGHT >= CONTAINER_HEIGHT) {
-            topPosition = topPosition - BOX_HEIGHT;
+        else if (topPosition + BOX_HEIGHT == CONTAINER_HEIGHT) {
+            topPosition = topPosition - SPEED;
         }
         if (leftPosition == 0) {
-            leftPosition = leftPosition + BOX_WIDTH;
+            leftPosition = leftPosition + SPEED;
         }
-        else if (leftPosition == CONTAINER_WIDTH) {
-            leftPosition = leftPosition - BOX_WIDTH - SPEED;
-        }
-        else if (leftPosition + BOX_WIDTH >= CONTAINER_WIDTH) {
-            leftPosition = leftPosition - BOX_WIDTH;
+        else if (leftPosition + BOX_WIDTH == CONTAINER_WIDTH) {
+            leftPosition = leftPosition - SPEED;
         }
 
         if (boxList.length === 0) {
             box.style.top = `${topPosition}px`;
             box.style.left = `${leftPosition}px`;
-            boxContainer.append(box);
-            boxList.push(box);
+            parent.append(box);
+            boxList.push({ boxElement: box, positionX: leftPosition, positionY: topPosition, directionX: 1, directionY: 1 });
+
         }
 
         else {
             // check if box overlaps with other boxes
-            for (let j = 0; j < boxList.length; j++) {
-                let topPositionAnother = parseInt(boxList[j].style.top);
-                let leftPositionAnother = parseInt(boxList[j].style.left);
+            for (let j = 0; j < boxList.length - 1; j++) {
+                let leftPositionAnother = boxList[j].positionX;
+                let topPositionAnother = boxList[j].positionY;
 
                 // get top & left position until they do not overlap 
                 while (Math.abs(topPositionAnother - topPosition) <= BOX_HEIGHT) {
                     if (Math.abs(leftPositionAnother - leftPosition) > BOX_WIDTH) {
                         break;
                     }
-                    topPosition = getPosition(CONTAINER_HEIGHT);
+                    topPosition = getPosition(CONTAINER_HEIGHT, BOX_HEIGHT);
                 }
                 while (Math.abs(leftPositionAnother - leftPosition) <= BOX_WIDTH) {
                     if (Math.abs(topPositionAnother - topPosition) > BOX_WIDTH) {
                         break;
                     }
-                    leftPosition = getPosition(CONTAINER_WIDTH);
+                    leftPosition = getPosition(CONTAINER_WIDTH, BOX_WIDTH);
                 }
 
             }
             box.style.top = `${topPosition}px`;
             box.style.left = `${leftPosition}px`;
-            boxContainer.append(box);
-            boxList.push(box);
+            parent.append(box);
+            // drawBox();
+            boxList.push({ boxElement: box, positionX: leftPosition, positionY: topPosition, directionX: 1, directionY: 1 });
         }
 
     }
     return boxList;
 }
+
+// function drawBox(){}
 
 /**
  *returns a random value in the range of height/width 
@@ -113,59 +112,43 @@ function createBoxes(boxContainer) {
  * @param {*} coordinate
  * @return {*} random position
  */
-function getPosition(coordinate) {
+function getPosition(coordinate, boxSize) {
     // set the postition in the multiples of SPEED
-    return Math.floor(Math.random() * coordinate / SPEED) * SPEED;
-
+    return Math.floor(Math.random() * (coordinate - boxSize) / SPEED) * SPEED;
 }
 
 function startApp(list) {
     let boxList = list;
     setInterval(animateBox, 50);
     function animateBox() {
+        debugger;
+
         for (let i = 0; i < boxList.length; i++) {
             box = boxList[i];
-            let coordinateX = parseInt(box.style.left);
-            let coordinateY = parseInt(box.style.top);
-            let directionX = 1;
-            let directionY = 1;
+            let coordinateX = box.positionX;
+            let coordinateY = box.positionY;
+
             // check x coordinate
             if (coordinateX >= CONTAINER_WIDTH - BOX_WIDTH) {
-                directionX = -1;
-                coordinateX = coordinateX + SPEED * directionX;
-                box.style.left = `${coordinateX}px`;
-                // boxList[i] = box;
-
+                box.directionX = -1;
             }
             else if (coordinateX <= 0) {
-                directionX = 1;
-                coordinateX = coordinateX + SPEED * directionX;
-                box.style.left = `${coordinateX}px`;
-                // boxList[i] = box;
-
+                box.directionX = 1;
             }
 
             // check y coordinate
             if (coordinateY >= CONTAINER_HEIGHT - BOX_HEIGHT) {
-                directionY = -1;
-                coordinateY = coordinateY + SPEED * directionY;
-                box.style.top = `${coordinateY}px`;
-                // boxList[i] = box;
-
-
+                box.directionY = -1;
             }
             else if (coordinateY <= 0) {
-                directionY = 1;
-                coordinateY = coordinateY + SPEED * directionY;
-                box.style.top = `${coordinateY}px`;
-                // boxList[i] = box;
-
-
+                box.directionY = 1;
             }
-            coordinateX = coordinateX + SPEED * directionX;
-            coordinateY = coordinateY + SPEED * directionY;
-            box.style.left = `${coordinateX}px`;
-            box.style.top = `${coordinateY}px`;
+            coordinateX = coordinateX + SPEED * box.directionX;
+            coordinateY = coordinateY + SPEED * box.directionY;
+            box.positionX = coordinateX;
+            box.positionY = coordinateY;
+            box.boxElement.style.left = `${coordinateX}px`;
+            box.boxElement.style.top = `${coordinateY}px`;
             // boxList[i] = box;
 
 
